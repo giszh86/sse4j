47633c5f4e3bc28c71614b5896763d6263b77f60
charmdx@126.com <charmdx@126.com@145daeb5-2533-4dc9-4f9b-eacf64891307>
2011-03-29 04:13:44 +0000

    precess but not end

--- trunk/sse4j.http/src/org/sse/http/Locating.java
+++ trunk/sse4j.http/src/org/sse/http/Locating.java
@@ -6,15 +6,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.sse.ws.base.WSFilterGeoc;
+import org.sse.ws.base.WSPointF;
+import org.sse.ws.base.WSResult;
+
 public class Locating extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	org.sse.ws.Locating locating = new org.sse.ws.Locating();
-	
+
 	public Locating() {
 		super();
 	}
-
+	
+	//	<ws:geocoding>
+	//	  <arg0>
+	//	    <address></address>
+	//	    <key></key>
+	//	  </arg0>
+	//	</ws:geocoding>
+	//
+	//	<ws:reverseGeocoding>
+	//	  <arg0>
+	//	    <x></x>
+	//	    <y></y>
+	//	  </arg0>
+	//	</ws:reverseGeocoding>
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		//TODO
+		// 1 get parameters
+		boolean isgeoc = false;		
+
+		// 2 execute
+		WSResult result = null;
+		if (isgeoc) {
+			WSFilterGeoc geoc = new WSFilterGeoc();
+			
+			result = locating.geocoding(geoc);
+		} else {
+			WSPointF point = new WSPointF();
+			
+			result = locating.reverseGeocoding(point);
+		}
+
+		// 3 write
+		GZipWriter.write(result, response);
 	}
 }
--- trunk/sse4j.http/src/org/sse/http/Searching.java
+++ trunk/sse4j.http/src/org/sse/http/Searching.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.sse.ws.base.WSResult;
+
 public class Searching extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	org.sse.ws.Searching searching = new org.sse.ws.Searching();
@@ -14,7 +16,32 @@ public class Searching extends HttpServlet {
 		super();
 	}
 
+	//	<ws:poiInfo>
+	//	  <arg0>
+	//	    <id></id>
+	//	    <key></key>
+	//	  </arg0>
+	//	</ws:poiInfo>
+	//
+	//	<ws:search>
+	//	  <arg0>
+	//	    <count></count>
+	//	    <!--distance></distance>
+	//	    <geometryWKT></geometryWKT-->
+	//	    <key></key>
+	//	    <keyword></keyword>
+	//	    <preference></preference>
+	//	  </arg0>
+	//	</ws:search>
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		//TODO
+		// 1 get parameters
+
+		// 2 execute
+		WSResult result = null;
+
+		// 3 write
+		GZipWriter.write(result, response);
 	}
 }

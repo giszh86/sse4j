14fcc6ed4057ea44a247ad83af0a1bd53d78dd1e
charmdx <charmdx@126.com>
2011-04-12 02:30:10 +0000

    update hotmapper

--- sse4j/src/org/sse/util/EarthPos.java
+++ sse4j/src/org/sse/util/EarthPos.java
@@ -8,24 +8,9 @@ package org.sse.util;
 public class EarthPos {
 	public double xLon;
 	public double yLat;
-	private int width = 1;
-	private int height = 1;
 
 	public EarthPos(double xLon, double yLat) {
 		this.xLon = xLon;
 		this.yLat = yLat;
 	}
-
-	public void setBuffer(int width, int height) {
-		this.width = width;
-		this.height = height;
-	}
-
-	public boolean equals(Object pt) {
-		if (pt instanceof EarthPos)
-			return (Math.abs(this.xLon - ((EarthPos) pt).xLon) <= width && Math
-					.abs(this.yLat - ((EarthPos) pt).yLat) <= height);
-		return false;
-	}
-
 }

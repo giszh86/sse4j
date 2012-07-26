d5e89135251d793593cb81c5e73343764edb9140
charmdx <charmdx@126.com>
2012-07-26 08:25:35 +0000

    v1.7.0_b1

--- sse4j/src/main/java/org/sse/squery/STree.java
+++ sse4j/src/main/java/org/sse/squery/STree.java
@@ -1,11 +1,8 @@
 package org.sse.squery;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.vividsolutions.jts.geom.Envelope;
-import com.vividsolutions.jts.index.kdtree.KdNode;
-import com.vividsolutions.jts.index.kdtree.KdTree;
 import com.vividsolutions.jts.index.strtree.STRtree;
 
 /**
@@ -14,23 +11,16 @@ import com.vividsolutions.jts.index.strtree.STRtree;
  * 
  */
 public class STree {
-	private Object sidx = null;
+	private STRtree sidx = null;
 	private Envelope extent = null;
 
 	public STree(boolean isPoint) {
-		if (isPoint) {
-			sidx = new KdTree();
-		} else {
-			sidx = new STRtree();
-		}
+		sidx = new STRtree();
 	}
 
 	public void insert(Envelope envelope, Object key) {
-		if (sidx instanceof KdTree) {
-			((KdTree) sidx).insert(envelope.centre(), key);
-		} else {
-			((STRtree) sidx).insert(envelope, key);
-		}
+		sidx.insert(envelope, key);
+
 		if (extent == null)
 			extent = envelope;
 		else
@@ -38,22 +28,11 @@ public class STree {
 	}
 
 	public void build() {
-		if (sidx instanceof STRtree) {
-			((STRtree) sidx).build();
-		}
+		sidx.build();
 	}
 
 	List spatialFilter(Envelope envelope) {
-		if (sidx instanceof KdTree) {
-			List list = ((KdTree) sidx).query(envelope);
-			List result = new ArrayList(list.size());
-			for (int i = 0; i < list.size(); i++) {
-				result.add(((KdNode) list.get(i)).getData());
-			}
-			return result;
-		} else {
-			return ((STRtree) sidx).query(envelope);
-		}
+		return sidx.query(envelope);
 	}
 
 	Envelope getExtent() {

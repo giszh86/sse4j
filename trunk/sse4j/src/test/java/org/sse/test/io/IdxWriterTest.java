10e2c4a1e4a11fd2cbe20211510d4274922ef4d0
charmdx@126.com <charmdx@126.com@145daeb5-2533-4dc9-4f9b-eacf64891307>
2012-12-26 11:04:22 +0000

    repair IdxWriter using default IK analyzer

--- trunk/sse4j/src/test/java/org/sse/test/io/IdxWriterTest.java
+++ trunk/sse4j/src/test/java/org/sse/test/io/IdxWriterTest.java
@@ -3,6 +3,7 @@ package org.sse.test.io;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.sse.io.Enums.AnalyzerType;
 import org.sse.io.IdxWriter;
 import org.sse.geo.AttributeType;
 import org.sse.geo.Feature;
@@ -34,11 +35,11 @@ public class IdxWriterTest {
 		}
 
 		String idxpath = "D:/temp/parking";
-		IdxWriter iw = new IdxWriter();
+		IdxWriter iw = new IdxWriter(idxpath, AnalyzerType.SMARTCN);
 		List<String> fields = new LinkedList<String>();
 		fields.add("NAME");
 
-		iw.create(fc, fields, idxpath);
+		iw.create(fc, fields);
 
 	}
 }
